db3d0ca15602f9ad2dcda9dba148071cd3a9b72f
StewartBC <stewcapps@gmail.com>
2021-09-02 14:45:21 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ let groupIDs = [1397, 1455, 1373, 630, 1418, 604, 1663, 605, 1375, 1440, 1441, 6
 let products = [];
 let total = 0;
 
+$(document).keydown(function (event) {
+    var key = (event.keyCode ? event.keyCode : event.which);
+    if (key === 13) {
+        event.preventDefault();
+        setTimeout(function () {
+            searchCards();
+        }, 1);
+    }
+    if (key === 8) {
+        setTimeout(function () {
+            searchCards();
+        }, 1);
+    }
+});
+
 function searchCards() {
     $("#img1").empty();
     $("#img2").empty();
@@ -30,20 +45,20 @@ function searchCards() {
                     <div class="col-md-6">
 
                         <div class="row">
-                            <div class="btn-group btn-group-toggle" data-toggle="buttons">
-                                <label data-id=${product.productId} data-condition="NM" class="btn btn-secondary active condition">
-                                    <input class="condition" type="radio" name="options" autocomplete="off" active> NM
+                            <div class="btn-group btn-group-toggle pink-background" data-toggle="buttons">
+                                <label data-id=${product.productId} data-condition="NM" class="btn btn-secondary condition pink-background">
+                                    <input class="condition" type="radio" name="options" autocomplete="off"> NM
                                 </label>
-                                <label data-id=${product.productId} data-condition="LP" class="btn btn-secondary condition">
+                                <label data-id=${product.productId} data-condition="LP" class="btn btn-secondary condition pink-background">
                                     <input class="condition" type="radio" name="options" autocomplete="off"> LP
                                 </label>
-                                <label data-id=${product.productId} data-condition="MP" class="btn btn-secondary condition">
+                                <label data-id=${product.productId} data-condition="MP" class="btn btn-secondary condition pink-background">
                                     <input class="condition" type="radio" name="options" autocomplete="off"> MP
                                 </label>
-                                <label data-id=${product.productId} data-condition="HP" class="btn btn-secondary condition">
+                                <label data-id=${product.productId} data-condition="HP" class="btn btn-secondary condition pink-background">
                                     <input class="condition" type="radio" name="options" autocomplete="off"> HP
                                 </label>
-                                <label data-id=${product.productId} data-condition="DMG" class="btn btn-secondary condition">
+                                <label data-id=${product.productId} data-condition="DMG" class="btn btn-secondary condition pink-background">
                                     <input class="condition" type="radio" name="options" autocomplete="off"> DMG
                                 </label>
                             </div>
@@ -61,36 +76,18 @@ function searchCards() {
     }
 }
 
-// function displayChosen(price) {
-//     $("#chosen").empty();
-//     products.forEach(product => {
-//         if (product.chosenCount > 0) {
-//             for (i = 0; i < product.chosenCount; i++) {
-//                 $("#chosen").append(`
-//                     <img class="footerImage" src="${product.imageUrl}" alt="${product.name}" data-price="${price}" data-id="${product.productId}">
-//             `);
-//             }
-//         }
-//     });
-//     $("#total").html(`$${total.toFixed(2)}`);
-// };
-
 $(document).on("click", ".footerImage", function () { 
     let id = $(this).attr("data-id");
     id = parseInt(id);
     let price = $(this).attr("data-price");
     price = parseFloat(price);
     total = total - price;
-    $("#total").html(`$${total.toFixed(2)}`);
+    if (total % 1 === 0) {
+        $("#total").html(`$${Math.round(total)}`);
+    } else {
+        $("#total").html(`$${total.toFixed(2)}`);
+    }
     $(this).remove();
-    // products.forEach(product => {
-    //     if (product.productId === id) {
-    //         console.log(product.price);
-    //         total = total - parseFloat(product.price);
-    //         product.chosenCount = product.chosenCount - 1;
-    //     }
-    // });
-    // displayChosen();
 });
 
 $(document).on("click", ".add", function () { 
@@ -111,11 +108,14 @@ $(document).on("click", ".add", function () {
                 $("#chosen").append(`
                 <img class="footerImage" src="${product.imageUrl}" alt="${product.name}" data-price="${price}" data-id="${product.productId}">
             `);
-            $("#total").html(`$${total.toFixed(2)}`);
+            console.log(total)
+            if (total % 1 === 0) {
+                $("#total").html(`$${Math.round(total)}`);
+            } else {
+                $("#total").html(`$${total.toFixed(2)}`);
+            }
             }
         });
-        // displayChosen(price);
-
     }
 
 });
@@ -160,7 +160,7 @@ $(document).on("click", ".condition", function () {
                     }
                 }).then(function (res) {
                     console.log(res)
-                    html = html + `Unlimited: ${res.results[0].lowPrice}<button data-id=${id} data-price=${res.results[0].lowPrice} type="button" class="btn btn-secondary add">Add</button>`;
+                    html = html + `Unlimited: ${res.results[0].lowPrice}<button data-id=${id} data-price=${res.results[0].lowPrice} type="button" class="btn btn-secondary add pink-background">Add</button>`;
                     if (result.results.length > 5) {
                         $.ajax({
                             type: "GET",
@@ -170,9 +170,9 @@ $(document).on("click", ".condition", function () {
                             }
                         }).then(function (res) {
                             if (groupId === 1663 || groupId === 630 || groupId === 635 || groupId === 1373 || groupId === 1389 || groupId === 1396 || groupId === 1434 || groupId === 1440 || groupId === 1441 || groupId === 1444) {
-                                html = html + `<br>1st: ${res.results[0].lowPrice}<button data-id=${id} data-price=${res.results[0].lowPrice} type="button" class="btn btn-secondary add">Add</button>`;
+                                html = html + `<br>1st: ${res.results[0].lowPrice}<button data-id=${id} data-price=${res.results[0].lowPrice} type="button" class="btn btn-secondary add pink-background">Add</button>`;
                             } else {
-                                html = html + `<br>Reverse: ${res.results[0].lowPrice}<button data-id=${id} data-price=${res.results[0].lowPrice} type="button" class="btn btn-secondary add">Add</button>`;
+                                html = html + `<br>Reverse: ${res.results[0].lowPrice}<button data-id=${id} data-price=${res.results[0].lowPrice} type="button" class="btn btn-secondary add pink-background">Add</button>`;
 
                             }      
                             $(`#${id}Price`).html(html);
@@ -217,9 +217,50 @@ groupIDs.forEach(id => {
                 product.chosenCount = 0;
                 product.condition = "NM";
                 product.price = 0;
+                product.set = 0;
+                if (product.groupId === 1663) {
+                    product.set = 0;
+                } else if (product.groupId === 604) {
+                    product.set = 1;
+                } else if (product.groupId === 605) {
+                    product.set = 2;
+                } else if (product.groupId === 635) {
+                    product.set = 3;
+                } else if (product.groupId === 630) {
+                    product.set = 4;
+                } else if (product.groupId === 1373) {
+                    product.set = 5;
+                } else if (product.groupId === 1441) {
+                    product.set = 6;
+                } else if (product.groupId === 1440) {
+                    product.set = 7;
+                } else if (product.groupId === 1374) {
+                    product.set = 8;
+                } else if (product.groupId === 1418) {
+                    product.set = 9;
+                } else if (product.groupId === 1396) {
+                    product.set = 10;
+                } else if (product.groupId === 1434) {
+                    product.set = 11;
+                } else if (product.groupId === 1389) {
+                    product.set = 12;
+                } else if (product.groupId === 1444) {
+                    product.set = 13;
+                } else if (product.groupId === 648) {
+                    product.set = 14;
+                } else if (product.groupId === 1375) {
+                    product.set = 15;
+                } else if (product.groupId === 1397) {
+                    product.set = 16;
+                } else if (product.groupId === 1372) {
+                    product.set = 17;
+                } else if (product.groupId === 1455) {
+                    product.set = 18;
+                }
                 product.name = product.name.toLowerCase();
                 products.push(product);
             });
+            products.sort((a, b) => a.set - b.set);
         });
     }
     });
